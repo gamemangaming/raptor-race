2a577fe11475d92bb64b481b5b2bb2631514d1db
game man <gamemangaming82@gmail.com>
2023-04-04 17:06:20 -0700

    add moving platform

--- scripts/player.gd
+++ scripts/player.gd
@@ -5,6 +5,7 @@ extends CharacterBody2D
 
 @onready var sprite = $AnimatedSprite2D
 @onready var jump_sound = $jumpSound
+@onready var camera = $"/root/World/Camera2D"
 
 var active = true
 var jumps_remaining = 2
@@ -18,26 +19,29 @@ func _physics_process(delta):
 	velocity.y += gravity * delta
 	
 	if active:
+		# Update the camera position
+		camera.position = position
+
 		# Reset the plater after landing
 		if was_jumping and is_on_floor():
 			was_jumping = false
 			jumps_remaining = 2
 			sprite.play("run")
 			jump_pitch = 1.0
-		
+
 		# Handle jumping
 		if Input.is_action_just_pressed("jump") and jumps_remaining > 0:
 			jumps_remaining -= 1
 			was_jumping = true
 			velocity.y = -jump_power
-			
+
 			if jumps_remaining == 1:
 				sprite.play("jump")
 			else:
 				sprite.play("double jump")
-				
+
 			jump_sound.set_pitch_scale(jump_pitch)
 			jump_sound.play()
 			jump_pitch += 0.2
-	
+
 	move_and_slide()
